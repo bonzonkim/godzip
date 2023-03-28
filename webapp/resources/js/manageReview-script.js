227bfc21955726474151e7ee6a6cf3725c901198
bonzonkim <flrhyme9@gmail.com>
2023-03-28 11:48:54 +0900

    t asdf

--- webapp/resources/js/manageReview-script.js
+++
@@ -1,8 +0,0 @@
-const boxlist = document.querySelectorAll("input[type=checkbox]");
-boxlist[0].addEventListener("click",(box)=>{
-  const status = box.target.checked;
-
-  for(let checkbox of boxlist){
-    checkbox.checked = status;
-  }
-})
